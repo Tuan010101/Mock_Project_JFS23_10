33cbcd94600190dc9e2d6ddd76bc8ade8f02d8f3
Phùng Hoàng Phú Lộc <phunghoangphuloc@gmail.com>
2024-03-04 15:45:42 +0700

    add function active email

--- Mock_Project_JFS23_10/src/main/java/fa/training/common/SendMail.java
+++ Mock_Project_JFS23_10/src/main/java/fa/training/common/SendMail.java
@@ -14,6 +14,21 @@ import fa.training.entities.Contact;
 
 public class SendMail {
 
+	public static void sendActiveEmail(String email, String code, String codeLink) throws AddressException, MessagingException {
+		
+		String linkConfirm = "http://localhost:8080/Mock_Project_JFS23_10/active_email?email=" + email + "&codeLink=" + codeLink;
+
+		String bodyEmail = "<h3>Chào bạn</h3>"
+				+ "Xác nhận Email:" 
+				+ "<blockquote>"
+						+ "<b> Code active: </b>" + code + "<br>"
+						+ "<b> Link active: </b>" + "<a href='" + linkConfirm + "'>" + linkConfirm + "</a>" + "<br>"
+				+ "</blockquote>"
+				+ "<h3>Trân trọng!</h3>";
+ 
+		sendHTML(email, "Active Email from WEB VEGEFOODS", bodyEmail);
+	}
+
 	public static void sendContact(Contact contact) throws AddressException, MessagingException {
 		String fullName = contact.getFullName();
 		String email = contact.getEmail();
@@ -22,11 +37,14 @@ public class SendMail {
 
 		String bodyEmail = "<h3>Chào bạn <b>" + fullName + "</b></h3>"
 				+ "Chúng tôi đã nhận được Email liên hệ từ bạn.Nội dung phản hồi:" 
-				+ "<blockquote><i><b>" + subject
-				+ "</b><br>" + message + "</i></blockquote>"
-				+ "Email sẽ được chúng tôi phản hồi trong thời gian sớm nhất." + "<h3>Trân trọng!</h3>";
+				+ "<blockquote><i>"
+						+ "<b>" + subject + "</b><br>" 
+						+ message 
+				+ "</i></blockquote>"
+				+ "Email sẽ được chúng tôi phản hồi trong thời gian sớm nhất." 
+				+ "<h3>Trân trọng!</h3>";
 
-		sendHTML(email, subject, bodyEmail);
+		sendHTML(email, "Contact from WEB VEGEFOODS", bodyEmail);
 	}
 
 	public static void sendHTML(String recipientAddress, String subject, String message)
@@ -50,7 +68,6 @@ public class SendMail {
 		mailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(recipientAddress));
 		mailMessage.setSubject(subject);
 		mailMessage.setContent(message, "text/html; charset=UTF-8");
-		
 
 		// Step3: Send mail
 		Transport transport = getMailSession.getTransport("smtp");
